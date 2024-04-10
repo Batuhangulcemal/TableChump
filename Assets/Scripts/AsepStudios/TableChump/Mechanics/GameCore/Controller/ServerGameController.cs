3bc00bbabc41d34395cacb85b5dfc0c2651e8610
Batuhan Gülcemal <52890428+Batuhangulcemal@users.noreply.github.com>
2024-04-10 14:42:46 +0300

    Beautify server game controller.

--- Assets/Scripts/AsepStudios/TableChump/Mechanics/GameCore/Controller/ServerGameController.cs
+++ Assets/Scripts/AsepStudios/TableChump/Mechanics/GameCore/Controller/ServerGameController.cs
@@ -5,27 +5,27 @@ namespace AsepStudios.TableChump.Mechanics.GameCore.Controller
 {
     public class ServerGameController : MonoBehaviour
     {
-        public static ServerGameController Instance;
+        private static ServerGameController _instance;
 
-        private GameController controller;
-        public static GameController Controller => GetController();
+        private GameController _controller;
+        public static GameController Controller
+        {
+            get
+            {
+                if (NetworkManager.Singleton.IsServer)
+                {
+                    return _instance._controller ??= new GameController();
+                }
+
+                Debug.Log("Client's can not access this component.");
+                return null;
+            }
+        }
 
         private void Awake()
         {
-            Instance = this;
-            controller = new GameController();
+            _instance = this;
         }
-
-        private static GameController GetController()
-        {
-            if (NetworkManager.Singleton.IsServer)
-            {
-                return Instance.controller;
-            }
-
-            Debug.Log("Client's can not access this component.");
-            return null;
-            
-        }
+        
     }
 }
